c61685d8e41c264d334615f903ddd6f8b585296a
Abhinav <abhinavjha98ald@gmail.com>
2018-12-22 13:35:28 +0530

    Started making the app

---
+++ controllers/users.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = function(_){
+	return{
+		SetRouting: function(router){
+			router.get('/',this.indexPage);
+			router.get('/signup',this.getSignUp);;
+		},
+		indexPage: function(req,res){
+			return res.render('index',{test:'This is a testttt'});
+		},
+		getSignUp: function(req,res){
+			return res.render('signup');
+		}
+	}
+}
